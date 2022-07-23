4852630fe5d09c948af0d3697d2eadd8adcb373e
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-07-23 19:06:57 +0300

    add login-reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,13 @@ import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
+import { loginReducer } from '../features/login/login-reducer';
+
 import { appReducer } from './app-reducer';
 
 const rootReducer = combineReducers({
   app: appReducer,
+  login: loginReducer,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
