b1208b627d4268d47839086de9c1261361897d1c
Ilua Kisialiou <iluxa91@inbox.ru>
2022-07-26 14:44:57 +0300

    add app toolbar

--- src/common/components/_NAVI_FOR_DEV/NAVI_FOR_DEV.tsx
+++ src/common/components/_NAVI_FOR_DEV/NAVI_FOR_DEV.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 import { ReturnComponentType } from '../../../types/ReturnComponentType';
@@ -7,12 +8,19 @@ import { RoutePath } from '../../enums/route-path';
 
 export const NaviForDev = (): ReturnComponentType => (
   <div>
-    <NavLink to={RoutePath.Error404}>404 </NavLink>-
-    <NavLink to={RoutePath.Profile}>Profile </NavLink>-
-    <NavLink to={RoutePath.Login}>Login </NavLink>-
-    <NavLink to={RoutePath.Register}>Register </NavLink>-
-    <NavLink to={RoutePath.ForgotPassword}>ForgotPassword </NavLink>-
-    <NavLink to={RoutePath.CheckEmail}>CheckEmail </NavLink>-
-    <NavLink to={RoutePath.CreateNewPassword}>CreateNewPassword </NavLink>-
+    <AppBar style={{ background: '#EBE0E9' }} position="static">
+      <Toolbar style={{ justifyContent: 'space-around' }}>
+        <Typography variant="h5">It-incubator</Typography>
+        <Box>
+          <NavLink to={RoutePath.Error404}>404 </NavLink>-
+          <NavLink to={RoutePath.Profile}> Profile </NavLink>-
+          <NavLink to={RoutePath.Login}> Login </NavLink>-
+          <NavLink to={RoutePath.Register}> Register </NavLink>-
+          <NavLink to={RoutePath.ForgotPassword}> ForgotPassword </NavLink>-
+          <NavLink to={RoutePath.CheckEmail}> CheckEmail </NavLink>-
+          <NavLink to={RoutePath.CreateNewPassword}>CreateNewPassword </NavLink>-
+        </Box>
+      </Toolbar>
+    </AppBar>
   </div>
 );
